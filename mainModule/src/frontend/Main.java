761774a5293b1ce4575a370b9216937c5ad819e5
mota <mepuedeayudar@hotmail.com>
2015-10-17 17:27:12 -0300

    Merge remote-tracking branch 'origin/master'

--- mainModule/src/frontend/Main.java
+++ mainModule/src/frontend/Main.java
@@ -1,8 +1,53 @@
 package frontend;
 
+import backend.worldBuilding.Location;
+import backend.worldBuilding.Player;
+import backend.worldBuilding.World;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.Collection;
 
-public class Main{
+
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception{
+        Group root = new Group();
+        Canvas canvas = new Canvas(600,600);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+        Player jugador1 = new Player("A");
+        Player jugador2 = new Player("B");
+        World mundo = new World(5,5,jugador1,jugador2);
+
+
+    }
+
+
+    public Location drawLocationToGridLocation(Double x,Double y){
+        Location gridLocation = new Location(0,0);
+
+        Double auxY = Math.floor(y/75);
+        Double auxX = auxY.intValue() % 2 == 0? Math.floor(x/100):Math.floor((x-50)/100);
+
+        gridLocation.setY(auxY.intValue());
+        gridLocation.setX(auxX.intValue());
+
+        return gridLocation;
+    }
 
     public static void main(String[] args) {
+        launch(args);
     }
 }
